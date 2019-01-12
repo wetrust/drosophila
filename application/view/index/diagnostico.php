55543eb93c1442418b0e5571445a2040d2b3d054
Cristopher Castro <cristophernic@live.cl>
2019-01-12 19:31:58 -0300

    .

--- application/view/index/diagnostico.php
+++ application/view/index/diagnostico.php
@@ -3,11 +3,16 @@
 </div>
 <div class="container">
     <div class="card">
-        <img class="card-img-top" src="..." alt="Card image cap">
-        <div class="card-body">
-            <h1 class="card-title">Diagnóstico inical para determinar riesgo de Drosophila suzukii</h1>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="card-body p-0">
+            <h1 class="card-title mt-4 mx-4">Diagnóstico inical para determinar riesgo de Drosophila suzukii</h1>
+            <div class="media mt-2 mx-4">
+                <div class="media-body">
+                    <h5 class="mt-0 mb-1">Bienvenido! </h5>
+                    lorem ipsum huaso chileno
+                    <a href="#" class="btn mt-2 btn-outline-primary">Realizar <br>Diagnóstico</a>
+                </div>
+                <img alt="Generic placeholder image" src="img/campesino.svg" class="ml-3 w-50">
+            </div>
         </div>
     </div>
 </div>
